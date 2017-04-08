52b36445e619e46b97c1679f848d49bf0a13189e
Mahbub Siddique <mahbub.siddique@yahoo.com>
2017-04-08 15:01:54 -0400

    added workWithString.java for reverse string functions

---
+++ src/main/java/com/tt/app/workWithString.java
@@ -0,0 +1,31 @@
+package com.tt.app;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class workWithString {
+
+	public static void main(String[] args) throws IOException {
+		//System.out.println("Reverse string");		
+		String str; 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		 
+		System.out.println("Enter a String: ");
+		str = br.readLine();
+		
+		workWithString ws = new workWithString();
+		ws.reverseEverything(str);
+
+	}
+	public void reverseEverything(String str){
+		System.out.print("Here is the string given:");
+		System.out.println(str);
+		String reverseString = new StringBuffer(str).reverse().toString();
+		System.out.print("Here is the string reversed:");
+		System.out.println(reverseString);
+		
+	}//end of reverseEverything
+
+
+}
