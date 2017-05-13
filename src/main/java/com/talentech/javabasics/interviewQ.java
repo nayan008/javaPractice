49ffa4145ebd658003ab2df3b1edb6a31124e0b3
Mahbub Siddique <mahbub.siddique@yahoo.com>
2017-05-13 10:16:40 -0400

    Updated interview q code

--- src/main/java/com/talentech/javabasics/interviewQ.java
+++ src/main/java/com/talentech/javabasics/interviewQ.java
@@ -1,29 +1,46 @@
 package com.talentech.javabasics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class interviewQ {
-	private void replaceDuplicate(){
-		
+	public void replaceDuplicate(){
+		int x [] = {1,2,2,3,3,4,5,6,7,7,8,9};
+		for (int i=0; i<x.length-1; i++){
+			if (x[i]==x[i+1]){
+				x[i]=0;		
+				System.out.println(x[i]);
+			}//end if		
+		}//end for
 	}//end replaceDuplicate
 	
-	public void fizzBuzz(){
+	public List<String> fizzBuzz(int[]x){
 		String temp=" ";
-		int[] x = {3,5,9,15,16,4,2};
+		//int[] x = {3,5,9,15,16,4,2};
+		List<String> fb = new ArrayList<String>();
 		for (int i=0;i<x.length; i++){
 			if (x[i]%3==0&&x[i]%5==0){
 				temp = "FizzBuzz";
+				fb.add(temp);
 			}//end of if
 			else if (x[i]%3==0){
 				temp = "Buzz";
+				fb.add(temp);
 			}// end of else if
 			else if (x[i]%5==0){
-				temp = "Fizz";			
+				temp = "Fizz";	
+				fb.add(temp);
 			}//end of else if
 			else
 			{
-				temp = "None";
+				temp = Integer.toString(x[i]);
+				fb.add(temp);
 			}
-			System.out.println(temp);
+			//System.out.println(temp);
 		}// end of for
+		
+		//System.out.println(fb);
+		return fb;
 		
 		
 	}// end divFiveThree
@@ -31,6 +48,8 @@ public class interviewQ {
 	public void swapWithoutThirdVariable(){
 		int a = 5;
 		int b = 3;
+		System.out.println("a: "+a);
+		System.out.println("b: "+b);
 		
 		a = a+b;
 		b = a-b;
--- src/main/java/com/talentech/javabasics/interviewQCall.java
+++ src/main/java/com/talentech/javabasics/interviewQCall.java
@@ -3,8 +3,16 @@ package com.talentech.javabasics;
 public class interviewQCall {
 
 	public static void main(String[] args) {
+		//passing array for FizzBuzz
 		interviewQ iq = new interviewQ();
-		iq.fizzBuzz();
+		int[] x = {3,5,9,15,16,4,27};
+		System.out.println(iq.fizzBuzz(x));
+		
+		System.out.println("===============================");
+		iq.replaceDuplicate();
+		System.out.println("===============================");
+		
+		//call to swap values without third variable
 		iq.swapWithoutThirdVariable();
 
 	}
